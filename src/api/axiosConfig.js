07808505b1522feb9896ecad87684aab1a8a01c4
Nishadsha91 <muthumattilnishad91@gmail.com>
2025-11-03 22:07:20 +0530

    last

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,14 +1,69 @@
 
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: "https://corsproxy.io/?http://184.73.8.191:8000/api",
+// });
+
+
+// // Request interceptor: attach access token
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("accessToken"); 
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// // Response interceptor: handle 401 (access token expired)
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
+//     if (error.response && error.response.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+
+//       const refreshToken = localStorage.getItem("refreshToken");
+//       if (refreshToken) {
+//         try {
+//           const res = await axios.post("http://184.73.8.191:8000/api/token/refresh/", {
+//             refresh: refreshToken,
+//           });
+//           localStorage.setItem("accessToken", res.data.access); 
+//           originalRequest.headers.Authorization = `Bearer ${res.data.access}`;
+//           return axiosInstance(originalRequest);
+//         } catch (err) {
+//           console.error("Refresh token failed", err);
+       
+//           localStorage.removeItem("accessToken");
+//           localStorage.removeItem("refreshToken"); 
+//           window.location.href = "/login";
+//         }
+//       }
+//     }
+
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default axiosInstance;
+
+
 import axios from "axios";
 
+const BASE_URL = "https://corsproxy.io/?http://184.73.8.191:8000/api";
+
 const axiosInstance = axios.create({
-  baseURL: "http://184.73.8.191:8000/api",
+  baseURL: BASE_URL,
 });
 
 // Request interceptor: attach access token
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("accessToken"); 
+    const token = localStorage.getItem("accessToken");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -16,6 +71,8 @@ axiosInstance.interceptors.request.use(
   },
   (error) => Promise.reject(error)
 );
+
+
 
 // Response interceptor: handle 401 (access token expired)
 axiosInstance.interceptors.response.use(
@@ -28,22 +85,20 @@ axiosInstance.interceptors.response.use(
       const refreshToken = localStorage.getItem("refreshToken");
       if (refreshToken) {
         try {
-          const res = await axios.post("http://127.0.0.1:8000/api/token/refresh/", {
+          const res = await axios.post(`${BASE_URL}/token/refresh/`, {
             refresh: refreshToken,
           });
-          localStorage.setItem("accessToken", res.data.access); 
+          localStorage.setItem("accessToken", res.data.access);
           originalRequest.headers.Authorization = `Bearer ${res.data.access}`;
           return axiosInstance(originalRequest);
         } catch (err) {
           console.error("Refresh token failed", err);
-       
           localStorage.removeItem("accessToken");
-          localStorage.removeItem("refreshToken"); 
+          localStorage.removeItem("refreshToken");
           window.location.href = "/login";
         }
       }
     }
-
     return Promise.reject(error);
   }
 );
